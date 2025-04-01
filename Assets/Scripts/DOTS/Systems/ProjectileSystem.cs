ad4453cf35112dbceec9f04753da847481586064
unduke2 <unduke2@tuta.io>
2025-04-01 14:10:05 +0100

    Refactored ProjectileSystem

--- Assets/Scripts/DOTS/Systems/ProjectileSystem.cs
+++ Assets/Scripts/DOTS/Systems/ProjectileSystem.cs
@@ -3,7 +3,6 @@ using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
-using UnityEngine;
 
 public partial struct ProjectileSystem : ISystem
 {
@@ -28,55 +27,43 @@ public partial struct ProjectileSystem : ISystem
             DroneTransforms = droneTransforms,
             DroneEntities = droneEntities,
         };
-        job.ScheduleParallel();
 
-        
-        
+        job.ScheduleParallel();
     }
+
+
     [BurstCompile]
     public partial struct ProjectileCollisionJob : IJobEntity
     {
-        // Read-only arrays of drones
-        [ReadOnly]
-        public NativeArray<LocalTransform> DroneTransforms;
-        [ReadOnly]
-        public NativeArray<Entity> DroneEntities;
+
+        [ReadOnly] public NativeArray<LocalTransform> DroneTransforms;
+        [ReadOnly] public NativeArray<Entity> DroneEntities;
 
         public EntityCommandBuffer.ParallelWriter ECB;
+
         public float DeltaTime;
 
-        // Each Execute() is called once per projectile
         void Execute(Entity entity, [EntityIndexInQuery] int entityInQueryIndex,
                      ref ProjectileData projectileData,
                      ref LocalTransform transform)
         {
-            // 1) Update projectile movement
             transform.Position += projectileData.Velocity * DeltaTime;
 
-            // 2) Decrement lifetime
             projectileData.DestroyTimer -= DeltaTime;
+
             if (projectileData.DestroyTimer <= 0)
             {
                 ECB.DestroyEntity(entityInQueryIndex, entity);
                 return;
             }
 
-            // 3) Collision check against all drones (naïve approach)
             for (int i = 0; i < DroneTransforms.Length; i++)
             {
-                float3 dronePos = DroneTransforms[i].Position;
-                float dist = math.distance(transform.Position, dronePos);
-
-                // Example collision radius
+                float dist = math.distance(transform.Position, DroneTransforms[i].Position);
                 if (dist < 25f)
                 {
-                    // Destroy projectile
                     ECB.DestroyEntity(entityInQueryIndex, entity);
-
-                    // Destroy this drone
                     ECB.DestroyEntity(entityInQueryIndex, DroneEntities[i]);
-
-                    // No need to check other drones
                     return;
                 }
             }
