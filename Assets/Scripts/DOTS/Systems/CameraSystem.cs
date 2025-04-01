4271405cb56001c10f46af489e72420ab0cc5157
unduke2 <unduke2@tuta.io>
2025-04-01 13:38:32 +0100

    Refactored CameraSystem

--- Assets/Scripts/DOTS/Systems/CameraSystem.cs
+++ Assets/Scripts/DOTS/Systems/CameraSystem.cs
@@ -10,40 +10,38 @@ public partial struct CameraSystem : ISystem
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
+
+
     public void OnUpdate(ref SystemState state)
     {
+        float deltaTime = SystemAPI.Time.DeltaTime;
         foreach (var (movementData, cameraData) in SystemAPI.Query<RefRO<MovementData>, RefRO<CameraData>>().WithPresent<CameraData>().WithAll<PlayerTag>())
         {
             var followTarget = state.EntityManager.GetComponentData<LocalToWorld>(cameraData.ValueRO.FollowTarget);
-            var deltaTime = SystemAPI.Time.DeltaTime;
-            // Smoothly lerp the camera to match the player
-            Vector3 oldPos = Camera.main.transform.position;
-            Quaternion oldRot = Camera.main.transform.rotation;
 
             Vector3 newPos = math.lerp(
-                oldPos,
+                Camera.main.transform.position,
                 followTarget.Position,
                 deltaTime * cameraData.ValueRO.FollowStep
             );
 
             Quaternion newRot = math.slerp(
-                oldRot,
+                Camera.main.transform.rotation,
                 followTarget.Rotation,
                 deltaTime * cameraData.ValueRO.FollowStep
             );
 
-            Camera.main.transform.position = math.lerp(Camera.main.transform.position, newPos, 1f);
-            Camera.main.transform.rotation = math.slerp(Camera.main.transform.rotation, newRot, 1f); 
+            Camera.main.transform.position = newPos;
+            Camera.main.transform.rotation = newRot;
 
             float velocityMagnitude = math.length(movementData.ValueRO.Velocity);
-
             float t = math.unlerp(0f, 300f, velocityMagnitude);
+            float smoothT = math.smoothstep(0, 1, 1 / t);
 
-            float smoothT = math.smoothstep(0, 1, 1/t);
-
-            Camera.main.fieldOfView = math.lerp(cameraData.ValueRO.MinFOV, cameraData.ValueRO.MaxFOV, smoothT);
+            Camera.main.fieldOfView = math.lerp(cameraData.ValueRO.MinFOV,
+                cameraData.ValueRO.MaxFOV,
+                smoothT
+                );
         }
     }
-
-    
 }
