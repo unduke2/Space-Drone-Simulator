438dbb13922ab8146d6269836ea0ab8835c24520
unduke2 <unduke2@tuta.io>
2025-04-01 14:12:01 +0100

    Refactored SpawnerSystem

--- Assets/Scripts/DOTS/Systems/SpawnerSystem.cs
+++ Assets/Scripts/DOTS/Systems/SpawnerSystem.cs
@@ -10,14 +10,14 @@ public partial struct SpawnerSystem : ISystem
         state.RequireForUpdate<SpawnerConfig>();
     }
 
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
         state.Enabled = false;
 
         var config = SystemAPI.GetSingleton<SpawnerConfig>();
-
-        Random rand = new Random((uint)UnityEngine.Random.Range(1, int.MaxValue));
+        var rand = new Random((uint)UnityEngine.Random.Range(1, int.MaxValue));
 
         for (int i = 0; i < config.Count; i++)
         {
@@ -25,20 +25,17 @@ public partial struct SpawnerSystem : ISystem
 
             if (i == 0)
             {
-
                 state.EntityManager.AddComponent<PlayerTag>(droneEntity);
                 state.EntityManager.SetComponentEnabled<PlayerMovementData>(droneEntity, true);
                 state.EntityManager.SetComponentEnabled<CameraData>(droneEntity, true);
-
             }
             else
             {
-
                 state.EntityManager.SetComponentEnabled<PlayerMovementData>(droneEntity, false);
                 state.EntityManager.SetComponentEnabled<CameraData>(droneEntity, false);
-
                 state.EntityManager.AddComponent<EnemyTag>(droneEntity);
             }
+
             state.EntityManager.SetComponentData(droneEntity, new LocalTransform
             {
                 Position = new float3
@@ -47,10 +44,9 @@ public partial struct SpawnerSystem : ISystem
                     y = rand.NextFloat(100, config.Bound),
                     z = rand.NextFloat(-config.Bound, config.Bound),
                 },
+                Rotation = quaternion.identity,
                 Scale = 1f,
-                Rotation = quaternion.identity
             });
-
         }
     }
 }
